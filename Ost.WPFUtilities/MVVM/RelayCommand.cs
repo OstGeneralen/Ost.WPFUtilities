d1008b1524d6f0fea5c094318e82428f0fb38124
OstGeneralen <kasper.esbjornsson@gmail.com>
2025-04-25 11:53:09 +0100

    Relay command utility

---
+++ Ost.WPFUtilities/MVVM/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Input;
+
+namespace Ost.WPFUtil.MVVM
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public RelayCommand(Action<object?> execute, Predicate<object?>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        public RelayCommand(Action execute, Func<bool> canExecute)
+            : this( (o) => execute(), (o) => canExecute() )
+        {
+        }
+        public RelayCommand(Action execute)
+            : this((o) => execute())
+        {
+        }
+
+        public bool CanExecute(object? parameter) => _canExecute != null ? _canExecute.Invoke(parameter) : true;
+        public void Execute(object? parameter) => _execute.Invoke(parameter);
+
+        private Action<object?> _execute;
+        private Predicate<object?>? _canExecute;
+    }
+}
